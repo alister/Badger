d317ccfd11255bbae8e48853b24d147802e2791e
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2015-03-03 17:33:21 +0100

    Added Twig provider and application config

--- src/Application.php
+++ src/Application.php
@@ -3,8 +3,10 @@
 namespace GravityMedia\Badger;
 
 use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\Debug\Debug;
 
 /**
  * Badger application
@@ -15,13 +17,19 @@ class Application extends \Silex\Application
 {
     /**
      * Create badge application
+     *
+     * @param array $config
      */
-    public function __construct()
+    public function __construct(array $config)
     {
         parent::__construct();
 
         foreach ($this->getDefaultProviders() as $provider) {
             $this->register($provider);
+        }
+
+        foreach ($config as $key => $value) {
+            $this[$key] = $value;
         }
     }
 
@@ -30,10 +38,22 @@ class Application extends \Silex\Application
      *
      * @return ServiceProviderInterface[]
      */
-    protected function getDefaultProviders() {
-        return array(
+    protected function getDefaultProviders()
+    {
+        return [
             new ServiceControllerServiceProvider(),
+            new TwigServiceProvider(),
             new UrlGeneratorServiceProvider()
-        );
+        ];
+    }
+
+    /**
+     * Configure application
+     */
+    protected function configure()
+    {
+        if ($this['debug']) {
+            Debug::enable();
+        }
     }
 }
